d40e708d2bfab47976d03c88163e717991cdc06d
Quincy <jjq.quirky@gmail.com>
2016-07-04 02:19:08 -0400

    added mountains and spacescenestuff

--- Code/Scenes/SpaceGame/SpaceScene.cs
+++ Code/Scenes/SpaceGame/SpaceScene.cs
@@ -5,6 +5,37 @@ namespace QuincyGameEnginePractice
 {
 	class SpaceScene : Scene
 	{
+		int remainingPixels;
 
+		public override void LoadContent()
+		{
+			remainingPixels = graphics.Viewport.Width;
+			BackgroundColor = Color.SaddleBrown;
+			Global.Ref.IsMouseVisible = true;
+		}
+
+		public override void Start()
+		{
+			do
+			{
+				new Mountain(world, ref remainingPixels);
+			}
+			while(remainingPixels > 0);
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			
+		}
+
+		public override void FixedUpdate(float fixedDelta)
+		{
+			
+		}
+
+		public override void UnloadContent()
+		{
+			
+		}
 	}
 }
