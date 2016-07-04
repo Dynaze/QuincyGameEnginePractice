c0c1c3972ce25f4ef96da28b8f9231b3dc3329c6
Quincy <jjq.quirky@gmail.com>
2016-07-04 00:59:32 -0400

    starting spacegame

---
+++ Code/Scenes/SpaceGame/Mountain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuincyGameEnginePractice
+{
+	class Mountain : GameObject
+	{
+		public Mountain() : base(true) { }
+
+
+	}
+}
